62c6cb33232e6d63b96948a6f2ed336a7dcee417
azjon <50624288+azjon@users.noreply.github.com>
2019-12-29 15:37:38 +0000

    Networking scripts and templates

--- Deployment Scripts/DeployNetworking.ps1
+++ Deployment Scripts/DeployNetworking.ps1
@@ -4,13 +4,71 @@ Param(
         [Parameter(Mandatory=$true)]
         $Identifier,
         [Parameter(Mandatory=$true)]
-        $HubRG,
-        [Parameter(Mandatory=$true)]
-        $RGName,
-        [Parameter(Mandatory=$true)]
         $DeployDR,
         [Parameter(Mandatory=$true)]
         $LocationPrimary,
         [Parameter(Mandatory=$true)]
         $LocationDR
     )
+
+    Write-Host "--------------------------------"
+    Write-Host "Section 0 - Set variables"
+    Write-Host "--------------------------------"
+
+    Write-Host "Set template and parameters paths"
+    $NetTemplateFilePrimary = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "..\nested templates\virtual network\VirtualNetworkPrimary.json"))
+    $NetTemplateFileDR = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "..\nested templates\virtual network\VirtualNetworkDR.json"))
+    $SpokeParamFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "..\parameters\$spoke.parameters.json"))
+
+    Write-Host "Checking whether template file for $spoke exists"
+
+    if(Test-Path $SpokeParamFile){
+        Write-Host "Parameters file for $spoke exists. Spoke will be deployed"
+        
+        Write-Host "--------------------------------"
+        Write-Host "Section 1 - Deploy networking"
+        Write-Host "--------------------------------"
+
+        #Deploy the hub or spoke
+        $RGNamePrimary = "rg-pr-" + $Identifier
+        $RGNameDR = "rg-dr-" + $Identifier 
+        $VNNamePrimary = "vn-pr-" + $Identifier
+        $VNNameDR = "vn-dr-" + $Identifier 
+        
+        New-AzResourceGroup -Name $RGNamePrimary -Location $LocationPrimary -Force
+
+        New-AzResourceGroupDeployment -TemplateParameterFile $SpokeParamFile `
+                                      -TemplateFile $NetTemplateFilePrimary `
+                                      -ResourceGroupName $RGNamePrimary `
+                                      -Identifier $Identifier `
+                                      -VNName $VNNamePrimary `
+                                      -EnvironmentIdentifier "pr"
+
+        if($DeployDR -eq $true){
+            New-AzResourceGroup -Name $RGNameDR -Location $LocationPrimary -Force
+
+            New-AzResourceGroupDeployment -TemplateParameterFile $SpokeParamFile `
+                                      -TemplateFile $NetTemplateFileDR `
+                                      -ResourceGroupName $RGNameDR `
+                                      -Identifier $Identifier `
+                                      -VNName $VNNameDR `
+                                      -EnvironmentIdentifier "dr"
+        }
+        <#
+        #Deploy VNET peering (for spokes only)
+        if($spoke -ne "hub"){
+
+            Write-Host "Deploy vnet peering between $identifier and $hubidentifier"
+
+            New-AzResourceGroupDeployment -TemplateFile $VNetPeerTemplateFile -ResourceGroupName $RGName -spokeIdentifier $Identifier -HubIdentifier $HubIdentifier -HubRG $HubRG
+        }
+        #>
+
+    }else{
+        Write-Host "No parameters file for $spoke exists. Spoke will not be deployed"
+        Write-Host "--------------------------------"
+        Write-Host "End"
+        Write-Host "--------------------------------"
+        
+        exit
+    }
---
+++ Deployment Scripts/Scrapbook.ps1
@@ -0,0 +1,7 @@
+cd "C:\Users\jon.clyde\Documents\GitHub\Infrastructure-as-Code-0.2\Deployment Scripts"
+
+Login-AzAccount
+
+Select-azsubscription "847f0a11-c32e-4c42-8d99-ae7bb0dd6b59"
+
+.\DeployNetworking.ps1 -Spoke "Hub" -Identifier "core" -DeployDR "true" -LocationPrimary "westeurope" -LocationDR "northeurope"
