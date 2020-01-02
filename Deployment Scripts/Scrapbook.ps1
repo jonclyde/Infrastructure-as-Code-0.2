d2dcdab2a838c62a6e656014522b51801b7f0b48
azjon <50624288+azjon@users.noreply.github.com>
2020-01-02 15:29:12 +0000

    Add various templates

--- Deployment Scripts/Scrapbook.ps1
+++ Deployment Scripts/Scrapbook.ps1
@@ -9,3 +9,9 @@ Select-azsubscription "847f0a11-c32e-4c42-8d99-ae7bb0dd6b59"
 .\DeployNetworking.ps1 -Spoke "spoke1" -Identifier "app1" -DeployDR "true" -LocationPrimary "westeurope" -LocationDR "northeurope" -IdentifierforHub "core"
 
 .\DeployNetworking.ps1 -Spoke "spoke2" -Identifier "rds" -DeployDR "true" -LocationPrimary "westeurope" -LocationDR "northeurope" -IdentifierforHub "core"
+
+New-AzResourceGroupDeployment -TemplateFile ".\Storage Account\storageaccount.json" -TemplateParameterFile "..\Parameters\StorageAccounts.parameters.json" -ResourceGroupName "az-core-recovery"
+
+New-AzResourceGroupDeployment -TemplateFile ".\Log Analytics\workspace.json" -ResourceGroupName "az-core-recovery" -identifier "core" -RandomInteger "43278423"
+
+New-AzResourceGroupDeployment -TemplateFile ".\Azure Backup\vault.json" -ResourceGroupName "az-core-recovery" -identifier "core" -RandomInteger "43278423" -DeployDR "true" -LocationPrimary "westeurope" -LocationDR "northeurope"
